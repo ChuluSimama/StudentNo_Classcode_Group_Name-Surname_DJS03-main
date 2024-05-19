51a3563492d0703a0ad1e905a4d66d22c23107df
ChuluSimmama <phatusimama@gmail.com>
2024-05-19 18:34:02 +0200

    Updated book preview click code

--- scripts.js
+++ scripts.js
@@ -184,38 +184,7 @@ document.querySelector('[data-search-form]').addEventListener('submit', (event) 
     document.querySelector("[data-list-items]").innerHTML = "";
     renderBooks(matches);
 
-
-
-    if (result.length < 1) {
-        document.querySelector('[data-list-message]').classList.add('list__message_show')
-    } else {
-        document.querySelector('[data-list-message]').classList.remove('list__message_show')
-    }
-
-    document.querySelector('[data-list-items]').innerHTML = ''
-    const newItems = document.createDocumentFragment()
-
-    for (const { author, id, image, title } of result.slice(0, BOOKS_PER_PAGE)) {
-        const element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
     
-        element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `
-
-        newItems.appendChild(element)
-    }
-
-    document.querySelector('[data-list-items]').appendChild(newItems)
     document.querySelector('[data-list-button]').disabled = (matches.length - (page * BOOKS_PER_PAGE)) < 1
 
     document.querySelector('[data-list-button]').innerHTML = `
@@ -250,26 +219,21 @@ document.querySelector('[data-list-button]').addEventListener('click', () => {
         fragment.appendChild(element)
     }
 
-    document.querySelector('[data-list-items]').appendChild(fragment)
-    page += 1
 })
 
+// Book preview click to show book details
 document.querySelector('[data-list-items]').addEventListener('click', (event) => {
     const pathArray = Array.from(event.path || event.composedPath())
     let active = null
 
     for (const node of pathArray) {
-        if (active) break
+        if (active) break;
 
-        if (node?.dataset?.preview) {
-            let result = null
-    
-            for (const singleBook of books) {
-                if (result) break;
-                if (singleBook.id === node?.dataset?.preview) result = singleBook
-            } 
-        
-            active = result
+        const id = node?.dataset?.preview;
+        for (const singleBook of bookObjects) {
+          if (singleBook.id === id) {
+            active = singleBook; // Set the active book
+          }
         }
     }
     
